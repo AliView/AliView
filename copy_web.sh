73a466dd591e3c211fd4d950560fe21a8e913bf5
Anders <anders@hemsnabeldatorn.(none)>
2014-05-15 21:58:06 +0200

    second edit

--- copy_web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# copy webpage to server
-rsync -av --update -e 'ssh -i /home/anders/.ssh/id_rsa  -p 64535' /home/anders/maven/AliView/web/* anders@ormbunkar.se:/srv/www/htdocs/aliview/
-
-rsync -av --update -e 'ssh -i /home/anders/.ssh/id_rsa  -p 64535' anders@ormbunkar.se:/srv/www/htdocs/aliview/index.html /home/anders/maven/AliView/web/
-
-
--- deployed/linux-version-0.9.5-beta/aliview/create-makeself-archive.sh
+++
@@ -1,5 +0,0 @@
-# make self installer
-makeself . ../aliview.install.run "Installer for AliView" ./install.sh 
-
-# make standard archive
-tar -czvf ../aliview.tgz *

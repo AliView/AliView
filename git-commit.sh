f8700292e50bb5c733070b18e993c67710b4ca83
Anders <anders@jobbsnabeldatorn.(none)>
2014-07-30 15:10:26 +0200

    version 1.11

--- git-commit.sh
+++ git-commit.sh
@@ -1,8 +1,14 @@
+#!/bin/sh
+
 # adding
 git add .
+read -p "Press any key..."
 # removing
 git add -u
+read -p "Press any key..."
 # commit local
 git commit -m "version 1.11"
+read -p "Press any key..."
 # commit server
 git push AliView master
+read -p "Press any key..."

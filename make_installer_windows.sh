72564536e04f64448c187e4caaa3bc30a010a99a
Anders Larsson <anders.larsson@icm.uu.se>
2018-02-04 12:25:01 +0100

    fixed path to windows inno-setup installer-creator

--- make_installer_windows.sh
+++ make_installer_windows.sh
@@ -12,10 +12,13 @@ INNO_SETUP_PROGRAM="$HOME/.wine/drive_c/Program Files/Inno Setup 5/iscc"
 LATEST_WIN_PATH=$(ls -d $PWD/target/windows-version-*)
 ln -s -f -T $LATEST_WIN_PATH $PWD/target/windows-latest
 
-wine $INNO_SETUP_PROGRAM "innosetupfil_for_Aliview_win.iss"
+wine "$INNO_SETUP_PROGRAM" "innosetupfil_for_Aliview_win.iss"
 
 # move static files
 rsync -av aliview-windows/* target/windows-latest/
+
+# make sure all files have right permissions
+chmod 755 -R target
 
 # move non installer files fo other dir
 mkdir target/windows-latest/without_installer_version

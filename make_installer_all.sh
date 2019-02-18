c41a1ab0088d2cccdece517031677182cb71668f
Anders Larsson <anders.larsson@icm.uu.se>
2019-02-18 08:22:49 +0100

    improve make-installer scripts

--- make_installer_all.sh
+++ make_installer_all.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -eu -o pipefail
+
+function print_error {
+	RED='\033[0;31m'
+    NC='\033[0m' # No Color
+
+    read line file <<<$(caller)
+
+    echo -e "${RED}"
+    echo "Error"
+    echo "An error occurred in line $line of file $file:" >&2
+    echo "---"
+    echo -e "${NC}"
+}
+trap print_error ERR
 
 ./make_installer_linux.sh
 ./make_installer_windows.sh

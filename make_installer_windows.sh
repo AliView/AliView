b1a8e606fa30745de6c06e3640fe20258905258c
Anders Larsson <anders.larsson@icm.uu.se>
2021-03-07 09:47:00 +0100

    use latest launch4j from bash script instead of in pom due to old version of maven launch4j plugin

--- make_installer_windows.sh
+++ make_installer_windows.sh
@@ -3,14 +3,25 @@ set -e
 
 echo "Make installer fow windows"
 
-#----------------------------------
-#prepare install-file for WINDOWS
-#----------------------------------
-
-# make link to latest version dir (this is what setup-script wants)
-INNO_SETUP_PROGRAM="$HOME/.wine/drive_c/Program Files/Inno Setup 5/iscc"
+# First make link to latest version dir (this is what this setup-script wants later)
 LATEST_WIN_PATH=$(ls -d $PWD/target/windows-version-*)
 ln -s -f -T $LATEST_WIN_PATH $PWD/target/windows-latest
+
+
+#---------------------------------
+#
+# First Create exe with launch4j
+#
+#---------------------------------
+$PWD/launch4j/launch4j $PWD/launch4j.config.xml
+
+
+#----------------------------------
+#
+# Second Create Windows installer with Inno Setup
+#
+#----------------------------------
+INNO_SETUP_PROGRAM="$HOME/.wine/drive_c/Program Files/Inno Setup 5/iscc"
 
 wine "$INNO_SETUP_PROGRAM" "innosetupfil_for_Aliview_win.iss"
 
